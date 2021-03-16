cfbfb08698b427f05e8ab79545df40639aafbb47
Johannes Herrmann <johannes.herrmann@e-ntegration.de>
2021-03-16 02:06:06 +0100

    check in dist folder

--- dist/covid-local-incidents.js
+++ dist/covid-local-incidents.js
@@ -11,16 +11,19 @@ module.exports = function (red) {
                     .get("https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?f=json&where=AdmUnitId%3D" + config.region + "&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=county%20asc&outSR=102100&resultOffset=0&resultRecordCount=500&resultType=standard&cacheHint=true")
                     .then(function (res) {
                     send({
-                        local_7_day_incidents: res.data.features[0].properties.cases7_per_100k,
-                        regional_7_day_incidents: res.data.features[0].properties.cases7_bl_per_100k,
-                        local_name: res.data.features[0].properties.county,
-                        regional_name: res.data.features[0].properties.BL
+                        payload: {
+                            region_7_day_incidents: res.data.features[0].attributes.cases7_per_100k,
+                            state_7_day_incidents: res.data.features[0].attributes.cases7_bl_per_100k,
+                            region_name: res.data.features[0].attributes.county,
+                            state_name: res.data.features[0].attributes.BL
+                        }
                     });
                     done();
-                });
+                })
+                    .catch(function () { return done(); });
             });
         }
         return CovidLocalIncidents;
     }());
-    red.nodes.registerType('time-based-dimmer', CovidLocalIncidents);
+    red.nodes.registerType('covid-local-incidents', CovidLocalIncidents);
 };
